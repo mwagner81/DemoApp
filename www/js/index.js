ce5513c8e6866356a76677f79d320b8e065acffe
mwagner81 <wagner@idsolutions.at>
2013-10-08 12:33:22 +0200

    Plugin Test

--- www/js/index.js
+++ www/js/index.js
@@ -17,42 +17,4 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        app.addImei();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    },addImei: function() {
-        alert("Imei Function");
-        window.plugins.imei.get(function(imei) {
-            var imeiVal = document.getElementById('imei');
-            imeiVal.innerHTML = imei;
-        }, function() {
-            alert("Imei fail");
-        });        
-    }
-};
+}
